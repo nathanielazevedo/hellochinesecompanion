befe4db127af2897706d463e3e9a98c3a8708e2c
nathanielazevedo <nathanielazevedo@hotmail.com>
2022-11-25 16:45:06 -0500

    patterns added

--- src/components/Patterns.tsx
+++ src/components/Patterns.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const Patterns = () => {
-  return <div style={{ border: "solid white 1px", height: "100%" }}></div>;
+  const [border, setBorder] = useState("transparent");
+  const firstWord = "ni hao, ni zai nali ne";
+  const secondWord = "wo zai California";
+  const handleChange = (val: string) => {
+    if (val === secondWord) {
+      setBorder("green");
+    } else {
+      setBorder("red");
+    }
+  };
+  return (
+    <div style={{ width: "100vw", display: "flex", justifyContent: "center" }}>
+      <div
+        style={{
+          height: "100%",
+          width: "50vw",
+          display: "flex",
+          alignItems: "center",
+          marginTop: "20px",
+          flexDirection: "column",
+        }}
+      >
+        <Typography alignSelf="flex-start">{firstWord}</Typography>
+        <TextField
+          onChange={(evt) => handleChange(evt.target.value)}
+          style={{
+            borderWidth: "1px",
+            borderStyle: "solid",
+            borderColor: border,
+          }}
+        ></TextField>
+      </div>
+    </div>
+  );
 };
 
 export default Patterns;
